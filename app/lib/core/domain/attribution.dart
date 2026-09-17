114940fe52adbda7477ad1a4389570fc14d99b75
Arab_Theeb <theeb1230@gmail.com>
2026-09-17 14:54:22 +0300

    feat: model third-party attribution records

---
+++ app/lib/core/domain/attribution.dart
@@ -0,0 +1,6 @@
+class Attribution {
+  const Attribution({required this.component, required this.license, required this.notice});
+  final String component;
+  final String license;
+  final String notice;
+}
