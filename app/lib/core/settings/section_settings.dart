aa65659ff468b8dafdb53746b4b14421024fb0ea
Arab_Theeb <theeb1230@gmail.com>
2026-09-17 18:04:06 +0300

    fix(settings): keep at least one product section visible

--- app/lib/core/settings/section_settings.dart
+++ app/lib/core/settings/section_settings.dart
@@ -5,14 +5,25 @@ class SectionSettings {
       : _enabled = {
           for (final id in SectionId.values) id: id != SectionId.optional,
           ...?enabled,
-        };
+        } {
+    _ensureVisibleSection();
+  }
 
   final Map<SectionId, bool> _enabled;
 
   bool isEnabled(SectionId id) => _enabled[id] ?? false;
 
-  void setEnabled(SectionId id, bool value) => _enabled[id] = value;
+  void setEnabled(SectionId id, bool value) {
+    _enabled[id] = value;
+    _ensureVisibleSection();
+  }
 
   List<SectionId> get visibleSections =>
       SectionId.values.where(isEnabled).toList(growable: false);
+
+  void _ensureVisibleSection() {
+    if (!SectionId.values.any(isEnabled)) {
+      _enabled[SectionId.cinema] = true;
+    }
+  }
 }
