228b5051d44f74662ebcd95fd36b8a424f706fc5
Arab_Theeb <theeb1230@gmail.com>
2026-09-20 00:04:48 +0300

    BUILD-1.33 support download lifecycle updates in memory

--- app/lib/core/data/in_memory_downloads.dart
+++ app/lib/core/data/in_memory_downloads.dart
@@ -7,5 +7,7 @@ class MemoryDownloadsRepository implements DownloadsRepository {
   @override
   Future<void> enqueue(DownloadJob job) async => _jobs[job.id] = job;
   @override
+  Future<void> update(DownloadJob job) async => _jobs[job.id] = job;
+  @override
   Future<void> remove(String id) async => _jobs.remove(id);
 }
